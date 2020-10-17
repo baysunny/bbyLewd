3e5fe926f0ddd2b639e195e36ed8c2668afb73b9
baysunny <baysunnyway@gmail.com>
2020-10-17 20:34:32 +0700

    status

--- bot.py
+++ bot.py
@@ -1,153 +1,40 @@
+from datetime import datetime
 import discord
-import random
 
 
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
-
-
-l = "https://discordapp.com/oauth2/authorize?client_id=758150057370451978&scope=bot&permissions=0"
-token = read_token()
+token = "NzU4MTUwMDU3MzcwNDUxOTc4.X2qwQw.f4hIHfMr42IwoJkl8XwZDU4JevU"
 client = discord.Client()
-
-short_memory = [
-]
-morning_greeting = {}
-
+lewd_server_id = 757865220604690443
+test_server_id = 739864997865455626
+list_server = [lewd_server_id, test_server_id]
+member_id = [736028616764424195, 762590005314715659]
 
 @client.event
-async def on_member_update(before, after):
-    n = after.nick
-    if n:
-        if str(before.id) == "736028616764424195":
-            last = before.nick
-            if last:
-                await after.edit(nick=last)
+async def on_ready():
+    print(f"logged in as {client.user}\n")
 
 
 @client.event
 async def on_message(message):
-    # server_owner = message.guild.owner
+    test_server = client.get_guild(test_server_id)
 
-    sender = message.author
-    msg = str(message.content).lower()
-
-    if str(sender.id) != "758150057370451978":
-        # print("--received new message--")
-        # print(f"auth id: {sender.id}")
-        # print(f"sender : {sender.display_name}")
-        # print(f"message: {msg}")
-        random_greeting = ["hi", "hello", "hiya", "annyeong", "hiya!"]
-        if msg[:5] == "ohayo":
-            sender_name = str(sender.display_name)
-            if sender_name not in morning_greeting:
-                morning_greeting[sender_name] = [msg, 1]
-                answers = ["ohayoo", "ohayouu",
-                           f"{sender_name} ohayoo!", f"{sender_name} ohayo~",
-                           f"{sender_name} ohayou"]
-                answer = random.choice(answers)
-                print(f"answer : {answer}")
-                await message.channel.send(answer)
-            else:
-                if morning_greeting[sender_name][1] < 2:
-                    await message.channel.send("are you lonely?")
-                elif morning_greeting[sender_name][1] == 2:
-                    answers = ["don't try me please", "don't spam!",
-                               "don't spam please!", "you don't try"]
-                    answer = random.choice(answers)
-                    print(f"answer : {answer}")
-                    await message.channel.send(answer)
-                elif morning_greeting[sender_name][1] == 3:
-                    answers = ["okai i'm tired", "you hurting me",
-                               "mama help!", "good bye :'(", "//whipeu whipeu//"]
-                    answer = random.choice(answers)
-                    print(f"answer : {answer}")
-                    await message.channel.send(answer)
-                else:
-                    await message.channel.send(f"[{sender_name}] spam [{morning_greeting[sender_name][1]}times]")
-                morning_greeting[sender_name][1] += 1
-
-        elif msg[:6] == "mornin":
-
-            sender_name = str(sender.display_name)
-            if sender_name not in morning_greeting:
-                morning_greeting[sender_name] = [msg, 1]
-                answers = ["morning", "gud morning!",
-                           f"gud morning {sender_name}!", f"gud moaning {sender_name}",
-                           f"good MOANing {sender_name}"]
-                answer = random.choice(answers)
-                print(f"answer : {answer}")
-                await message.channel.send(answer)
-            else:
-                if morning_greeting[sender_name][1] < 2:
-                    await message.channel.send("Hi")
-                elif morning_greeting[sender_name][1] == 2:
-                    answers = ["don't try me please", "don't spam!",
-                               "don't spam please!", "you don't try"]
-                    answer = random.choice(answers)
-                    print(f"answer : {answer}")
-                    await message.channel.send(answer)
-                elif morning_greeting[sender_name][1] == 3:
-                    answers = ["okai i'm tired", "you hurting me",
-                               "mama help!", "good bye :'(", "//whipeu whipeu//"]
-                    answer = random.choice(answers)
-                    print(f"answer : {answer}")
-                    await message.channel.send(answer)
-                else:
-                    await message.channel.send(f"[{sender_name}] spam [{morning_greeting[sender_name][1]}times]")
-                morning_greeting[sender_name][1] += 1
-
-        elif msg in random_greeting:
-            temp = random.choice([sender.display_name, ""])
-            answer = f"{random.choice(random_greeting)} {temp}!"
-            await message.channel.send(answer)
-
-        else:
-            pass
+    # elif message.content.lower == "!out!":
+    #     await client.close()
+    # print(test_server.id)
+    # print(message.channel)
 
 
-print("running...")
+@client.event
+async def on_member_update(before, after):
+    now = datetime.now()
+    server = client.get_guild(test_server_id)
+    channel = client.get_channel(739864997865455629)
+
+    if after.status != before.status:
+        print(f"[{now.strftime('%H:%M:%S')} status changed]")
+        print(f"-{before.nick} : {after.status}\n")
+        member = server.get_member(before.id)
+        if member is not None:
+            await channel.send(f"{now.strftime('%H:%M:%S')}:[{member.status}]{member.nick}")
+
 client.run(token)
-
-#
-#
-
-#
-#
-# banned_users = []
-#
-#
-# @client.event
-# async def on_message(message):
-#
-#     author = str(message.author)
-#     msg = str(message.content).lower()
-#     print(f"user:{author}")
-#     print(f"msg :{msg}")
-#     print("list banned user:")
-#     for user in banned_users:
-#         print(user, len(user))
-#     if author != "bbyLewd#2503":
-#         # print(message.guild.owner.display_name)
-#         if str(message.author.id) in banned_users:
-#             print(f"{author} sent message and got deleted")
-#             await message.channel.purge(limit=1)
-#         else:
-#             if msg[0:4].lower() == "hug ":
-#                 user = str(msg[6:-1])
-#                 banned_users.append(user)
-#                 print(f"msg :{msg}")
-#                 print(f"banned user :{user}")
-#                 print(len(user))
-#                 print(f"{author} was added to banned list")
-#                 print(banned_users)
-#                 await message.channel.send(f"{user} added")
-#             else:
-#                 if msg == "hi":
-#                     await message.channel.send(f"hi")
-#
-#             print("========")
-#
-#
