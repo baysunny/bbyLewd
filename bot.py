e7791fa6a5699803eaf9945d77640a2c6739191b
baysunny <baysunnyway@gmail.com>
2020-10-19 15:28:29 +0700

    massive updated

--- bot.py
+++ bot.py
@@ -1,5 +1,30 @@
 import discord
 from datetime import datetime
+import requests
+import json
+import random
+
+
+def get_gif(search):
+    api_key = "LIVDSRZULELA"
+    lmt = 8
+    r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % api_key)
+
+    if r.status_code == 200:
+        anon_id = json.loads(r.content)["anon_id"]
+    else:
+        anon_id = ""
+
+    r = requests.get(
+        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s&anon_id=%s" %
+        (search, api_key, lmt, anon_id))
+    gifs = []
+    if r.status_code == 200:
+        top_8gifs = json.loads(r.content)
+        for i in range(len(top_8gifs['results'])):
+            url = top_8gifs['results'][i]['media'][0]['gif']['url']
+            gifs.append(url)
+    return random.choice(gifs)
 
 
 token = "NzU4MTUwMDU3MzcwNDUxOTc4.X2qwQw.f4hIHfMr42IwoJkl8XwZDU4JevU"
@@ -10,11 +35,17 @@ servers = {
     "test-server": 739864997865455626
 }
 channels = {
-    "log-channel": 739864997865455629
+    "log-channel":     739864997865455629,
+    "bbyLewd-channel": 762607426582085642
 }
 
 bot_id = 758150057370451978
 member_id = [736028616764424195, 762590005314715659]
+
+gif_keys = {
+    "server-left": random.choice(["anime cry", "anime bye", "anime sad"]),
+    "server-joined": random.choice(["anime hello", "anime hi", "anime happy"]),
+}
 
 
 @client.event
@@ -25,25 +56,95 @@ async def on_ready():
 @client.event
 async def on_message(message):
     now = datetime.now()
+    acts = ["hug", "slap", "kiss", "fuck", "lick", "snipe", "kill", "hide", "sneak"]
+
     if message.author.id == bot_id:
         pass
     elif str(message.content).lower() == "hi":
-        await message.channel.send("hello")
         await message.channel.send(f"{now.strftime('%H:%M:%S')} -- hi")
-    # print(test_server.id)
-    # print(message.channel)
+    else:
+        messages = str(message.content).lower().split()
+        mentioned_members = message.mentions
+        print("\n======= new message")
+        print(f"{message.author.display_name}: {message.content}")
+        print(f"author   : {message.author.display_name}")
+        print(f"mentioned: {mentioned_members} | {len(mentioned_members)}")
+        print(f"message  : {message.content} | {len(messages)}")
+        print(f"messages : {messages}")
+
+        if len(mentioned_members) > 1 or len(mentioned_members) == 0:
+            pass
+        else:
+            if len(messages) > 1:
+                if messages[0] in acts:
+                    if messages[1][2:-1] == str(mentioned_members[0].id):
+                        embed = discord.Embed(
+                            title=f"{message.author.display_name} {messages[0]} {mentioned_members[0].display_name}",
+                            description="im confused",
+                            color=0x00ff00)
+                        embed.set_image(url=get_gif(f"anime {messages[0]}"))
+                        await message.channel.send(embed=embed)
+                    else:
+                        print("error 1")
+                        print(messages[1][2:-1])
+                        print(mentioned_members[0].id)
+                else:
+                    print(f"error 2: {messages[0]}")
+            else:
+                print(f"error 3: {len(messages)}")
+
+
+@client.event
+async def on_member_join(member):
+    # server = client.get_guild(servers["lewd-server"])
+    for channel in member.guild.channels:
+        if channel.id == channels["bbyLewd-channel"] or channel.id == channels["log-channel"]:
+            description = random.choice(
+                ["who are you?",
+                 "free whisshy whooshy for a week",
+                 f"say hi to {member.display_name}",
+                 "annyeong!"]
+            )
+            embed = discord.Embed(
+                title=f"{member.display_name} joined the server",
+                description=description,
+                color=0x00ff00)
+            embed.set_image(url=get_gif(gif_keys["server-joined"]))
+            print("\n======= server update")
+            print(f"{member.display_name} joined the server")
+            await channel.send(embed=embed)
+
+
+@client.event
+async def on_member_remove(member):
+    # server = client.get_guild(servers["lewd-server"])
+    for channel in member.guild.channels:
+        if channel.id == channels["bbyLewd-channel"] or channel.id == channels["log-channel"]:
+            description = random.choice(
+                ["bye bye",
+                 "yay",
+                 f"feel lonely",
+                 "shoo shoo"]
+            )
+            embed = discord.Embed(
+                title=f"{member.display_name} left the server!",
+                description=description,
+                color=0x00ff00)
+            embed.set_image(url=get_gif(gif_keys["server-left"]))
+            print("\n======= server update")
+            print(f"{member.display_name} left the server")
+            await channel.send(embed=embed)
 
 
 @client.event
 async def on_member_update(before, after):
     now = datetime.now()
     channel = client.get_channel(channels["log-channel"])
-    await channel.send("test")
-    await channel.send(channel)
-    # if channel is not None:
-    #     if after.status != before.status:
-    #         print(f"[{now.strftime('%H:%M:%S')} status changed]")
-    #         print(f"-{before.nick} : {after.status}\n")
-    #         await channel.send(f"{now.strftime('%H:%M:%S')} ---:--- [{after.status}] {before.nick}")
+    if channel is not None:
+        if after.status != before.status:
+            print(f"[{now.strftime('%H:%M:%S')} status changed]")
+            print(f"-{before.nick} : {after.status}\n")
+            await channel.send(f"{now.strftime('%H:%M:%S')} ---:--- [{after.status}] {before.nick}")
+
 
 client.run(token)
