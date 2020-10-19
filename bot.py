6480ef43c85b514e6e70996bbfe9fa4e0defe6a5
baysunny <baysunnyway@gmail.com>
2020-10-19 07:16:01 +0700

    i hate you

--- bot.py
+++ bot.py
@@ -4,10 +4,16 @@ from datetime import datetime
 
 token = "NzU4MTUwMDU3MzcwNDUxOTc4.X2qwQw.f4hIHfMr42IwoJkl8XwZDU4JevU"
 client = discord.Client()
+
+servers = {
+    "lewd-server": 757865220604690443,
+    "test-server": 739864997865455626
+}
+channels = {
+    "log-channel": 739864997865455629
+}
+
 bot_id = 758150057370451978
-lewd_server_id = 757865220604690443
-test_server_id = 739864997865455626
-list_server = [lewd_server_id, test_server_id]
 member_id = [736028616764424195, 762590005314715659]
 
 
@@ -19,7 +25,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     now = datetime.now()
-    test_server = client.get_guild(test_server_id)
     if message.author.id == bot_id:
         pass
     elif str(message.content).lower() == "hi":
@@ -31,12 +36,15 @@ async def on_message(message):
 @client.event
 async def on_member_update(before, after):
     now = datetime.now()
-    server = client.get_guild(test_server_id)
-    channel = client.get_channel(739864997865455629)
-    if channel is not None:
-        if after.status != before.status:
-            print(f"[{now.strftime('%H:%M:%S')} status changed]")
-            print(f"-{before.nick} : {after.status}\n")
-            await channel.send(f"{now.strftime('%H:%M:%S')} ---:--- [{after.status}] {before.nick}")
+    channel = client.get_channel(channels["log-channel"])
+
+    try:
+        if channel is not None:
+            if after.status != before.status:
+                print(f"[{now.strftime('%H:%M:%S')} status changed]")
+                print(f"-{before.nick} : {after.status}\n")
+                await channel.send(f"{now.strftime('%H:%M:%S')} ---:--- [{after.status}] {before.nick}")
+    except Exception as e:
+        await channel.send(str(e))
 
 client.run(token)
